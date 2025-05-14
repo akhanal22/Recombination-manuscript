f7cbc6b9b9091ebe7ef748c1d0e8e39d3730064e
Ashmita Khanal <92552834+akhanal22@users.noreply.github.com>
2025-05-14 11:55:56 -0500

    Update 09-genodb.sh

--- variant_calling/09-genodb.sh
+++ variant_calling/09-genodb.sh
@@ -1,3 +1,6 @@
+Before running this file, all.sample.map file is created which is basically first column with sample name and second with its location : while read -r i; 
+    do printf "$i\tgvcf/$i.g.vcf\n"; done < names.txt > all.sample_map. chromosome.list is the file listing all the chromosomes in reference genome which i generated using grep command and removed the ">" sign using sed command.
+
 #!/bin/bash
 #SBATCH -J genodb
 #SBATCH -N 1
@@ -9,6 +12,6 @@
 #SBATCH --export=ALL
 
 gatk --java-options "-Xmx4g -Xms4g" GenomicsDBImport \
-    --sample-name-map all.sample.map \
+    --sample-name-map all.sample_map \
     --genomicsdb-workspace-path genodb \
     -L chromosome.list
